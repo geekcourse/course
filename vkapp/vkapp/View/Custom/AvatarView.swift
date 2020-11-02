77721fcb986fbf97319c623ea4d117e417b5639b
geekcourse <1@1.ru>
2020-11-02 23:35:23 +0300

    lesson 4. Не понял как добавлять subview, like, alphcontrol

---
+++ vkapp/vkapp/View/Custom/AvatarView.swift
@@ -0,0 +1,21 @@
+//
+//  AvatarView.swift
+//  vkapp
+//
+//  Created by gorby on 02.11.2020.
+//  Copyright © 2020 gorby. All rights reserved.
+//
+
+import UIKit
+
+class AvatarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
