12bc3fc3fbf1d00bb3e2fc8a303f23ff6e60fd41
geekcourse <1@1.ru>
2020-11-05 00:27:05 +0300

    lesson-5

--- vkapp/vkapp/View/Custom/AvatarView.swift
+++ vkapp/vkapp/View/Custom/AvatarView.swift
@@ -8,14 +8,77 @@
 
 import UIKit
 
-class AvatarView: UIView {
+@IBDesignable class AvatarView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //MARK : SubView
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+
+        return imageView
+    }()
+
+    lazy var shadowView: UIView = {
+        let view = UIView()
+
+        return view
+    }()
+    
+    //MARK : - Init
+
+    @IBOutlet weak var image : UIImageView! {
+        didSet {
+            setNeedsDisplay()
+        }
     }
-    */
-
+    
+    var shadowView : UIView!
+    
+    @IBInspectable var shadowColor : UIColor = UIColor.black
+    @IBInspectable var shadowOffset : CGSize = CGSize(width: 10, height: 10)
+    @IBInspectable var shadowRadius : CGFloat = 10
+    @IBInspectable var shadowOpacity : Int = 50
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let _ = image else {
+            return
+        }
+        self.backgroundColor = .clear
+        self.layer.cornerRadius = self.frame.width / 2
+        image.layer.cornerRadius = self.frame.width / 2
+        
+        
+        
+        shadowView = UIView(frame: self.frame)
+        shadowView.layer.shadowColor = shadowColor.cgColor
+        shadowView.layer.shadowOffset = shadowOffset
+        shadowView.layer.shadowRadius = shadowRadius
+        shadowView.layer.shadowOpacity = Float(shadowOpacity) / 100
+        
+        if self.backgroundColor == .clear {
+            addSubview(shadowView)
+            self.layer.shadowColor = shadowColor.cgColor
+            self.layer.shadowOffset = shadowOffset
+            self.layer.shadowRadius = shadowRadius
+            self.layer.shadowOpacity = Float(shadowOpacity) / 100
+        }
+    }
+    
+    //TODO так и не понял как добавить subView, чтобы тень применялась как отдельный View
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        guard let _ = shadowView else {
+            return
+        }
+        shadowView.frame = bounds
+        
+    }
+    
 }
---
+++ vkapp/vkapp/View/Custom/LetterPicker.swift
@@ -0,0 +1,21 @@
+//
+//  LetterPicker.swift
+//  vkapp
+//
+//  Created by gorby on 04.11.2020.
+//  Copyright © 2020 gorby. All rights reserved.
+//
+
+import UIKit
+
+class LetterPicker: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
